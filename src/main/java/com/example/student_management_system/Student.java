84533f6e83c8d9f1781d8c119dfef459187d238b
Camilo Ordoñez <cronox20@gmail.com>
2024-05-21 09:07:46 -0400

    project done

--- src/main/java/com/example/student_management_system/Student.java
+++ src/main/java/com/example/student_management_system/Student.java
@@ -1,2 +1,51 @@
-package com.example.student_management_system;public class Student {
+package com.example.student_management_system;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Student {
+    private SimpleStringProperty id;
+    private SimpleStringProperty name;
+    private SimpleStringProperty major;
+
+    Student (String id, String name, String major) {
+        this.id = new SimpleStringProperty(id);
+        this.name =  new SimpleStringProperty(name);
+        this.major = new SimpleStringProperty(major);
+    }
+
+    public String getId() {
+        return id.get();
+    }
+
+    public SimpleStringProperty idProperty() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getMajor() {
+        return major.get();
+    }
+
+    public SimpleStringProperty majorProperty() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major.set(major);
+    }
 }
--- src/main/java/com/example/student_management_system/StudentController.java
+++ src/main/java/com/example/student_management_system/StudentController.java
@@ -1,14 +1,75 @@
 package com.example.student_management_system;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class HelloController {
+public class StudentController {
     @FXML
-    private Label welcomeText;
+    private Label title;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableView<Student> studentTable;
+
+    @FXML
+    private TableColumn<Student, String> idColumn;
+    @FXML
+    private TableColumn<Student, String> nameColumn;
+    @FXML
+    private TableColumn<Student, String> majorColumn;
+    @FXML
+    private TextField idField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField majorField;
+
+    private ObservableList<Student> studentData = FXCollections.observableArrayList();
+
+
+    @FXML
+    private void addStudent(ActionEvent event) {
+        // Create a new Student from text field data
+        // Add the new Student to the table
+        if (idField.getText().isEmpty() || nameField.getText().isEmpty() || majorField.getText().isEmpty()) {
+            System.out.println("Enter some data");
+        } else {
+            Student s = new Student(idField.getText(), nameField.getText(), majorField.getText());
+            studentData.add(s);
+            clearFields();
+        }
+    }
+    @FXML
+    private void deleteStudent(ActionEvent event) {
+        // Delete the selected Student from the table
+        try {
+            Student selectedItem = studentTable.getSelectionModel().getSelectedItem();
+            studentTable.getItems().remove(selectedItem);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    @FXML
+    public void initialize() {
+        // Initialize the table
+        title.setText("Student Management System");
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        majorColumn.setCellValueFactory(new PropertyValueFactory<>("major"));
+
+        studentTable.setItems(studentData);
+    }
+
+    public void clearFields () {
+        idField.clear();
+        nameField.clear();
+        majorField.clear();
     }
 }
